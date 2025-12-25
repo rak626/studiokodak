ff7e381f3db9939e979b986107e9de84bde7b145
Rakesh Ghosh <rakeshacot@gmail.com>
2025-12-25 19:18:47 +0530

    Profile config changes

--- src/util/profile.ts
+++ src/util/profile.ts
@@ -1,7 +1,7 @@
 export const Profile = {
     name: "Studio Kodak",
     about: String("Experience the magic of STUDIO KODAK, West Midnapore's premier Wedding photographer, capturing life's precious moments for 29 years. From enchanting Photo booth sessions to breathtaking Aerial photographer shots, our expert team creates unforgettable Wedding Portrait Studio masterpieces. Trust us to craft your special day into a timeless story through our Video production services. Book your unforgettable experience today and let the memories unfold!"),
-    whatsappNo: "917001074104",
+    whatsappNo: "918250617748",
     email: "studiokodak96@gmail.com",
     mobileNo: "8250617748",
     mobileNo2: "9434414382",
